a833df69c7181e4e5373d7933050978ca6b2a26e
Paolo Benetti <53307935+PaoloBenetti@users.noreply.github.com>
2026-07-10 17:27:33 +0200

    Update vector.py

--- src/core/vector.py
+++ src/core/vector.py
@@ -19,3 +19,25 @@ class Vector2D:
 
     def __str__(self):
         return f'Vettore di coordinate ( {self.x}, {self.y})'
+
+@dataclass
+class Polyline:
+    linea: list[Vector2D]
+
+    def __len__(self):
+        return len(self.linea)
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            cls = type(self)
+            return cls(self.linea[item])
+        return self.linea[item]
+
+    def __setitem__(self, key, value):
+        if not isinstance(value, Vector2D): # in fase ottimizzazione gli assert spariscono
+            raise TypeError(f"Expected Vector2D, got {type(value).__name__}")
+        if isinstance(key, slice):
+            cls = type(self)
+            return cls(self.linea[key])
+        else:
+            self.linea[key] = value
