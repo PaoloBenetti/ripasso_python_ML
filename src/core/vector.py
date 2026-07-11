395509ae72291825d8821035e6294e2eec9b8422
Paolo Benetti <53307935+PaoloBenetti@users.noreply.github.com>
2026-07-11 18:02:31 +0200

    Add vector addition and multiplication methods

--- src/core/vector.py
+++ src/core/vector.py
@@ -11,6 +11,37 @@ class Vector2D:
             return NotImplemented
         return self.x == other.x and self.y == other.y
 
+    def __add__(self, other): #versione semplice
+        if not isinstance(other, Vector2D):
+            return NotImplemented
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+    '''
+    def __add__(self, other): versione n dim
+        try:
+            pairs = itertools.zip_longest(self, other, fillvalue=0.0)
+            return Vector2D(a+b for a,b in pairs)
+        except TypeError:
+            return NotImplemented
+    '''
+
+    def __radd__(self, other):
+        # compatibilita per versione built-in sum
+        if other == 0:
+            return self
+        return self + other
+
+    def __mul__(self, other):
+        if not isinstance(other, (float, int)):
+            return NotImplemented
+        return Vector2D(self.x * other, self.y * other)
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __neg__(self):
+        return Vector2D(- self.x, -self.y)
+
     def __hash__(self):
         return hash((self.x, self.y))
 
