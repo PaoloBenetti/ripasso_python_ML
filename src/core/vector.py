97e8201453d19e179e226e7c6726ff24bbdee7c0
Paolo Benetti <53307935+PaoloBenetti@users.noreply.github.com>
2026-07-13 08:24:35 +0200

    Refactor Vector2D and Polyline classes with type hints

--- src/core/vector.py
+++ src/core/vector.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 from collections.abc import Sequence
 
@@ -7,12 +9,12 @@ class Vector2D:
     x: float
     y: float
 
-    def __eq__(self, other):
+    def __eq__(self, other:Vector2D) -> bool:
         if not isinstance(other, Vector2D):
             return NotImplemented
         return self.x == other.x and self.y == other.y
 
-    def __add__(self, other): #versione semplice
+    def __add__(self, other:Vector2D) -> Vector2D: #versione semplice
         if not isinstance(other, Vector2D):
             return NotImplemented
         return Vector2D(self.x + other.x, self.y + other.y)
@@ -26,46 +28,46 @@ class Vector2D:
             return NotImplemented
     '''
 
-    def __radd__(self, other):
+    def __radd__(self, other) -> Vector2D:
         # compatibilita per versione built-in sum
         if other == 0:
             return self
         return self + other
 
-    def __mul__(self, other):
+    def __mul__(self, other: int | float) -> Vector2D:
         if not isinstance(other, (float, int)):
             return NotImplemented
         return Vector2D(self.x * other, self.y * other)
 
-    def __rmul__(self, other):
+    def __rmul__(self, other) -> Vector2D:
         return self * other
 
-    def __neg__(self):
+    def __neg__(self) -> Vector2D:
         return Vector2D(- self.x, -self.y)
 
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash((self.x, self.y))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Vector2D({self.x!r},{self.y!r})'
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'Vettore di coordinate ( {self.x}, {self.y})'
 
 @dataclass
 class Polyline(Sequence):
     linea: list[Vector2D]
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.linea)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: int|slice) -> Vector2D|Polyline:
         if isinstance(item, slice):
             cls = type(self)
             return cls(self.linea[item])
         return self.linea[item]
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: int[slice], value: Vector2D|list[Vector2D]) -> None:
         if not isinstance(value, Vector2D): # in fase ottimizzazione gli assert spariscono
             raise TypeError(f"Expected Vector2D, got {type(value).__name__}")
         self.linea[key] = value
