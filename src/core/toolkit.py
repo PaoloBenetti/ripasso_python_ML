96fbfa89f2e6cb3ab17999aef6e75ab08892fc04
Paolo Benetti <53307935+PaoloBenetti@users.noreply.github.com>
2026-07-16 23:00:26 +0200

    Implement retry decorator for function execution
    Added a retry decorator to handle exceptions with multiple attempts.

--- src/core/toolkit.py
+++ src/core/toolkit.py
@@ -22,3 +22,33 @@ def log_calls(func):
         return risultato
     return call
 
+def retry(times=3, exceptions=(ValueError,)):
+    def tentativo(func):
+        @functools.wraps(func)
+        def esecuzione(*args, **kwargs): # effettuo i tentativi fino a ottenere un risultato
+            ult_ecc = None
+            for i in range(times):
+                try:
+                    return func(*args, **kwargs)
+                except exceptions as e: # accetta tuple
+                    ult_ecc = e
+                    print(f'Found exception {type(e).__name__} in trial {i+1}')
+
+            raise ult_ecc # altrimenti fallisce silenziosamente, meglio di no
+
+        return esecuzione
+    return tentativo
+
+def auto_repr(cls):
+
+    def n_repr(x):
+        lista_attr = [a for a in dir(x)
+                      if not callable(getattr(x, a)) and not a.startswith('_')]
+        msg = ', '.join(f'{a}={getattr(x,a)!r}' for a in lista_attr)
+        msg = f'{type(x).__name__}(' + msg + ')'
+        return msg
+
+    cls.__repr__ = n_repr
+
+    return cls
+
